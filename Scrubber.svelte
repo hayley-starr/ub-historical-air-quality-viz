e5a1f3e96b57c7f7ca7125a803c656e503f5d95e
Hayley Garment <hayley.garment@gmail.com>
2020-06-01 17:03:10 +0800

    basic animation of event dot

--- Scrubber.svelte
+++ Scrubber.svelte
@@ -1,6 +1,6 @@
 <script>
     import { onMount } from 'svelte';
-    import { styler, value, pointer, listen, transform, easing } from 'popmotion';
+    import { styler, value, pointer, listen, transform, easing, keyframes } from 'popmotion';
 
     export let currentFrame;
     export let pauseAnimation;
@@ -12,11 +12,62 @@
     const maxScrubberWidth = 1000;// width of scrubber in px
     let handleStyler;
 
+    //TEST!!
+    const eventPosition = 50;
+    const eventFadeBuffer = 10;
+    let eventStyler;
+
+
+    let eventKeyFramesExpand =  keyframes({
+        values: [ 
+            { scale: 1 },
+            { scale: 2 }
+        ],
+        times: [0, 1],
+        duration: 300,
+        easings: [easing.bounceOut]
+    });
+
+    let eventKeyFramesContract =  keyframes({
+        values: [ 
+            { scale: 2 },
+            { scale: 1 }
+        ],
+        times: [0, 1],
+        duration: 600,
+        easings: [easing.bounceIn]
+    });
+
+    //END_TEST!!
+
     $: {
        handleStyler && handleStyler.set('x', currentFrame);
+       if (currentFrame == eventPosition-eventFadeBuffer) {
+           highlightEvent();
+       }
+
+       if (currentFrame == eventPosition + eventFadeBuffer*2) {
+           reduceEvent();
+       }
+    }
+
+    const highlightEvent = () => {
+        eventKeyFramesExpand.start(style => {
+            eventStyler.set(style);
+        });
+    }
+
+    const reduceEvent = () => {
+        eventKeyFramesContract.start(style => {
+            eventStyler.set(style);
+        });
     }
 
     onMount(async () => {
+        const event = document.querySelector('.event');
+        eventStyler = styler(event);
+
+
         const handle = document.querySelector('.handle-hit-area');
         handleStyler = styler(handle);
         const handleX = value(0, (newX) => {
@@ -56,15 +107,24 @@
 
 
 <div class="scrubber">
+
     <div class='scrubber-controls'>
         <button on:click={handleStartAnimation}>Start</button>
         <button on:click={handlePauseAnimation}>Pause</button>
     </div>
+
     <div class="slider">
         <div class="range"></div>
+
         <div class="handle-container">
             <div class="handle-hit-area">
-            <div class="handle"></div>
+                <div class="handle"></div>
+            </div>
+        </div>
+
+        <div class="event-container">
+            <div class="event-hit-area">
+                <div class="event"></div>
             </div>
         </div>
     </div>
@@ -101,8 +161,6 @@
 
     .handle-container {
         position: absolute;
-        width: 20 + (30 * 2);
-        height: 30 + (30 * 2);
         top: 50%;
         left: 0%;
         transform: translateY(-50%) translateX(-50%);
@@ -119,6 +177,28 @@
         width: 5px; 
         height: 40px;
         cursor: pointer;
+    }
+
+     .event-container {
+        position: absolute;
+        top: 50%;
+        left: 50px; 
+        transform: translateY(-50%) translateX(-50%);
+    }
+
+    .event-hit-area {
+        padding: 30px;
+        width: 10px;
+        height: 10px;
+    }
+
+    .event {
+        background: darkslategrey;
+        width: 10px;
+        height: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: all .2s ease-in-out; 
     }
 
 
