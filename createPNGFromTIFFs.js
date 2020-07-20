d1cedd67ddef9c6fd79b6408571dc1b5379726d8
Hayley Garment <hayley.garment@gmail.com>
2020-07-20 17:58:17 +0800

    Creating a raster-based animation

---
+++ createPNGFromTIFFs.js
@@ -0,0 +1,151 @@
+import { contours } from 'd3-contour';
+import { geoTransform } from 'd3-geo';
+import { geoProject } from 'd3-geo-projection';
+import * as gdal from 'gdal';
+import fs from 'fs';
+import { PNG } from 'pngjs';
+import hexRgb from 'hex-rgb';
+import zeros from 'zeros'
+import savePixels from 'save-pixels'
+import * as turf  from 'turf';
+import { interpolateGreys } from 'd3-scale-chromatic';
+
+const NUM_INTERMEDIARY_FRAMES = 3;
+const NUM_CALCULATED_FRAMES = NUM_INTERMEDIARY_FRAMES + 1;
+
+let green_color = '#87e32b'; //green
+let red_color = '#f0004c'; //red
+let yellow_color = '#ebc505'; 
+let blue_color = '#027ef2';
+let white_color = '#ffffff';
+let orange_color = '#fc9d03';
+let purple_color = '#5e03fc';
+let dark_purple_color = '#4b1f7a';
+let black_color = '#050505';
+
+let COLOR_ARRAY = [
+    white_color, // 0
+    blue_color, // 1
+    blue_color, // 2
+    green_color, // 3
+    green_color, // 4
+    yellow_color, // 5
+    yellow_color, // 6
+    orange_color, // 7
+    orange_color, // 8
+    red_color, // 9
+    red_color, // 10
+    purple_color, // 11
+    purple_color, // 12
+    dark_purple_color, // 13
+    dark_purple_color // 14
+]
+
+
+// write geojson to file
+const writePNG = function(colorArray, width, height, filename) {
+
+    let rgb_array = new Array(width * height * 3);
+    let ofs = 0;
+
+    for (let i = 0; i < colorArray.length; i ++) {
+        let rgbColor = (colorArray[i]);
+        rgb_array[ofs++] = rgbColor[0];
+        rgb_array[ofs++] = rgbColor[1];
+        rgb_array[ofs++] = rgbColor[2];
+        rgb_array[ofs++] = 255;
+    }
+
+    let png = new PNG({
+        width: width,
+        height: height,
+        bitDepth: 8,
+        colorType: 6,
+      });
+
+    png.data = Buffer.from(rgb_array);
+    var buffer = PNG.sync.write(png, {
+        width: width,
+        height: height,
+        bitDepth: 8,
+        colorType: 6,
+      });
+
+    fs.writeFileSync(filename, buffer);
+
+    console.log('made a png!');
+}
+
+const getPixelColor = function(pixelValue) {
+    let pixelColor = pixelValue < 0 | pixelValue >= COLOR_ARRAY.length ? black_color : COLOR_ARRAY[pixelValue];
+    let rgbPixelColor = hexRgb(pixelColor, {format: 'array'});
+    return rgbPixelColor;
+}
+
+
+const createPNGFromFrame = function(inputFilename, outputFilename, frameId, prevBand) {
+    console.log('png-izing tif file: ' + inputFilename + '...');
+
+    let frameIdStart = (frameId - 1) * NUM_CALCULATED_FRAMES;
+
+    let gdalDataset = gdal.open(inputFilename);
+    let band = gdalDataset.bands.get(1);
+
+    let width = band.size.x-1; 
+    let height = band.size.y-1;
+    
+
+    //read data into an array of length widthxheight so that d3.contours can work
+    let array = band.pixels.read(0, 0, width, height);
+
+    let prevArray = prevBand ? prevBand.pixels.read(0, 0, width, height) : undefined;
+    let resultingFrames = []
+
+    for (let frm = 0; frm < NUM_CALCULATED_FRAMES; frm++) {
+        resultingFrames.push({frameId: frameIdStart + frm, colorArray: new Array(array.length) })
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        let currValue = array[i];
+        let prevValue = prevArray ? prevArray[i] : currValue;
+        let stepAmt = (currValue - prevValue) / (NUM_CALCULATED_FRAMES);
+
+        // calculate a value for each interpolated frame
+        for (let frm = 0; frm < NUM_CALCULATED_FRAMES; frm++) {
+            let interpolatedValue = Math.round(prevValue + (frm * stepAmt));
+           
+            let pixelColor = getPixelColor(interpolatedValue);
+            resultingFrames[frm].colorArray[i] = pixelColor;
+        }
+    }
+
+    // write each fresulting frame to a png file
+    for (let frm = 0; frm < NUM_CALCULATED_FRAMES; frm++) {
+        let pngFrame = resultingFrames[frm];
+        writePNG(pngFrame.colorArray, width, height, '' + outputFilename + '_' + frm + '.png');   
+    }
+    
+    return band;
+}
+
+const main = () => {
+
+    // get tif filenames from data directory
+    var files = fs.readdirSync('../ub-historical-air-quality-interpolation/R/frames');
+    var frameId = 1;
+    var prevBand;
+
+
+   //for each file, vectorize and save
+    files.forEach(filename => {
+        console.log(filename);
+
+        let inputFilename = '../ub-historical-air-quality-interpolation/R/frames/' + filename;
+        let outputFilename = 'pngs/img' + frameId;   
+        prevBand = createPNGFromFrame(inputFilename, outputFilename, frameId, prevBand);
+        frameId++;
+        return;
+    });
+}
+
+main();
